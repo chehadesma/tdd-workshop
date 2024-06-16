4bc5da8b1427302c72ad2fee894de01c348ca3eb
chehadesma <chehade@sma.de>
2024-06-16 14:55:17 +0200

    Update HelloWorldTest.cs

--- csharp/starter/DotnetStarter.Logic.Tests/HelloWorldTest.cs
+++ csharp/starter/DotnetStarter.Logic.Tests/HelloWorldTest.cs
@@ -1,13 +1,20 @@
+using System.Diagnostics;
 using Xunit;
 
 namespace DotnetStarter.Logic.Tests
 {
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
     public class HelloWorldTest
     {
         [Fact]
         public void It_Should_Run() => Assert.Equal(1, 1);
         
         [Fact]
-        public void Hello_ReturnsWorld() => Assert.Equal("Drink", "Food");
+        public void Hello_ReturnsWorld() => Assert.Equal("Drink", "Drink");
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
     }
 }
